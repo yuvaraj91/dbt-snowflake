23d5ae375666fbd3ad74c6387fe9b4c619516bfb
Yuvaraj Prem Kumar <46146150+yuvaraj91@users.noreply.github.com>
2023-03-26 21:28:17 +0200

    ascii char

--- snowflake/models/addresses.py
+++ snowflake/models/addresses.py
@@ -2,13 +2,13 @@ import base64
 import pandas as pd
 
 def encoding(x):
-    x = x.encode("ascii")
+    x = x.encode('ascii')
     base64_bytes = base64.b64encode(x)
-    return base64_bytes.decode("ascii")
+    return base64_bytes.decode('ascii', 'ignore')
     
     
 def model(dbt, session):
-    dbt.config(materialized="table")
+    dbt.config(materialized='table')
     
     df = dbt.source('public', 'addresses').to_pandas()
     df['STREET'] = df['STREET'].apply(lambda x: encoding(x) if pd.notnull(x) else x)
