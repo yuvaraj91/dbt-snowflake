1a41ae37d063f5284e47fe9e733473a81a73668d
Yuvaraj Prem Kumar <46146150+yuvaraj91@users.noreply.github.com>
2023-03-26 21:33:43 +0200

    remove testing file

--- tess.py
+++
@@ -1,9 +0,0 @@
-import base64
-
-def encoding(x):
-    x = x.encode("ascii")
-    base64_bytes = base64.b64encode(x)
-    return base64_bytes.decode("ascii")
-xx='117 east 57th street'
-print(xx)
-print(encoding(xx))
