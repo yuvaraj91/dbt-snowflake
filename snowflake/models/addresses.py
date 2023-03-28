fcaefec51ebae0486f4be3ab3c0408d1c764e0d3
Yuvaraj Prem Kumar <46146150+yuvaraj91@users.noreply.github.com>
2023-03-28 09:34:50 +0200

    debugging

--- snowflake/models/addresses.py
+++ snowflake/models/addresses.py
@@ -12,20 +12,14 @@ def model(dbt, session):
         materialized='incremental',
         unique_key='photographer_id',
         incremental_strategy='merge',
-        merge_update_columns= ['street']
+        merge_update_columns= ['STREET']
         )   
     df = dbt.source('public', 'addresses')
     
     if dbt.is_incremental:
         updated_at = f"select max(_airbyte_emitted_at) - INTERVAL '3 DAY' from public.addresses;"
-
-        df = df.filter(df.created >= session.sql(updated_at).collect()[0][0])
-        
-        df = df.to_pandas()
+        df = df.filter(df.created >= session.sql(updated_at).collect()[0][0]).to_pandas()
         df['CREATED'] = df['CREATED'].dt.tz_localize('UTC')
         df['MODIFIED'] = df['MODIFIED'].dt.tz_localize('UTC')
         df['STREET'] = df['STREET'].apply(lambda x: encoding(x) if pd.notnull(x) else x)
-        session.create_dataframe(df)
-
-        
     return df
