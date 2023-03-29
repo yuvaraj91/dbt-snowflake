681a456e97d45380a3bc6e735aa43f14038145f1
Yuvaraj Prem Kumar <46146150+yuvaraj91@users.noreply.github.com>
2023-03-29 10:25:17 +0200

    Final files!

--- snowflake/models/addresses.py
+++ snowflake/models/addresses.py
@@ -1,11 +1,5 @@
 import base64
-import pandas as pd
-
-def encoding(x):
-    x = x.encode('ascii', 'ignore')
-    base64_bytes = base64.b64encode(x)
-    return base64_bytes.decode('ascii')
-    
+from snowflake.snowpark.functions import md5    
     
 def model(dbt, session):
     dbt.config(
@@ -13,13 +7,13 @@ def model(dbt, session):
         unique_key='photographer_id',
         incremental_strategy='merge',
         merge_update_columns= ['STREET']
-        )   
+        )
+    delta_hours = dbt.config.get("delta_hours", '1 DAY')   
     df = dbt.source('public', 'addresses')
     
     if dbt.is_incremental:
-        updated_at = f"select max(_airbyte_emitted_at) - INTERVAL '3 DAY' from public.addresses;"
-        df = df.filter(df.created >= session.sql(updated_at).collect()[0][0]).to_pandas()
-        df['CREATED'] = df['CREATED'].dt.tz_localize('UTC')
-        df['MODIFIED'] = df['MODIFIED'].dt.tz_localize('UTC')
-        df['STREET'] = df['STREET'].apply(lambda x: encoding(x) if pd.notnull(x) else x)
+        updated_at = f"select max(_airbyte_emitted_at) - INTERVAL '{delta_hours}' from {dbt.this};"
+        df = df.filter(df.created >= session.sql(updated_at).collect()[0][0])
+        df = df.with_column("STREET", md5("STREET"))
+        
     return df
